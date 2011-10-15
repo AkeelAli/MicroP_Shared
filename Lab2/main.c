bd1651d2dad29cccba965a45d827eb9f11ef9eb7
Akeel <agilowen@gmail.com>
2011-10-15 18:19:04 -0400

    use this lab2 folder instead of lab2_latest/

---
+++ Lab2/main.c
@@ -0,0 +1,5 @@
+#include "LSM303DLH.h"
+
+int main(void){
+	return 0;	
+}
---
+++ Lab2/main.d
@@ -0,0 +1,15 @@
+main.o: main.c
+main.o: LSM303DLH.h
+main.o: HAL_LSM303DLH.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+main.o: C:\Keil\ARM\RV31\INC\stdint.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+main.o: C:\Users\Akeel\McGill\Fall 2011\microP\MicroP_Shared\Lab2\stm32f10x_conf.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_i2c.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+main.o: C:\Program Files (x86)\STMicroelectronics\iNEMO Suite\Firmware\Common\STM32F10xFWLib\v3_3_0\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
